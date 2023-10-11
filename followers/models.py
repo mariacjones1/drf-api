efb3f1b0e58eb16be48eee7e3abb031c85221947
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-10-11 14:06:09 +0000

    Refactor Post and Profile views to use generics

--- followers/models.py
+++ followers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Like(models.Model):
+class Follower(models.Model):
     """
     Follower model, related to 'owner' and 'followed'.
     'owner' is a User that is following a User.
