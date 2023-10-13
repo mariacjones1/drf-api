69c52a9420b62ee2143f54396db999c15d42c60d
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-10-13 17:38:16 +0000

    Add environment variables to allow use of API

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -63,7 +64,7 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
     '8000-mariacjones1-drfapi-4uctwmv5yim.ws-eu105.gitpod.io',
     'localhost',
-    'drf-api-mcj-6ae0938a9005.herokuapp.com',
+    os.environ.get('ALLOWED_HOST'),
 ]
 
 
@@ -111,9 +112,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
